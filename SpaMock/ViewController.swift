f93ba172bf0fed6bb9785a0a92dd8f6a14307781
kyo <kyo@h211-023.fsi.waikato.ac.nz>
2015-08-08 14:16:03 +1200

    github test

--- SpaMock/ViewController.swift
+++ SpaMock/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        //test of github
         // Do any additional setup after loading the view, typically from a nib.
     }
 
